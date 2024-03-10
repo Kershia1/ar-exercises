3db354b635364a9a5a63fc02c5bb39b187353a93
Kershia1 <insatiable.meerkat@gmail.com>
2024-03-10 04:52:16 +0000

    Add stores to db

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -11,3 +11,20 @@ puts "----------"
 # - Burnaby (annual_revenue of 300000, carries men's and women's apparel)
 # - Richmond (annual_revenue of 1260000 carries women's apparel only)
 # - Gastown (annual_revenue of 190000 carries men's apparel only)
+
+# note to self instead of 3 sep I can do one insert with each hash instead so from :
+
+# stores = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+
+# stores = Stores.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true )
+
+# stores = Stores.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false )
+
+# to an array of hashes, where each hash represents the attributes for one record. :
+
+stores = Store.create([
+  {name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true},
+
+ { name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true },
+
+ { name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false}])
